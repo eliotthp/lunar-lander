b7e37dde819e8065761113357689d5556452c870
Eliott Hendryx-Parker <eliott@sixfeetup.com>
2026-02-12 14:43:16 -0500

    Learning classes and dataclasses.

--- test.py
+++ test.py
@@ -1,32 +1,47 @@
+from dataclasses import dataclass
 import numpy as np
-import matplotlib.pyplot as plt
-import simulation as sim
-import guidance as gd
-import navigation as nav
-import environment as env
 
-# --- Constants & Environment ---
-r_moon = env.r_moon
-mu = env.mu
 
-S0 = [
-    14_878 + r_moon,
-    0,
-    0,
-    np.sqrt(mu / (r_moon + 14_878)) / (r_moon + 14_878),
-    15_240,
-]
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-t_vec = np.linspace(0, 500, 500)
+    def bark(self):
+        print("Woof!")
 
-LVLH = nav.polar_to_LVLH(S0)
-alt = nav.altitude(LVLH)
-z_cmd, _, _ = gd.poly_guidance(t_vec, [LVLH[0], 0, LVLH[1], 0], 500)
-x_cmd, _, _ = gd.poly_guidance(t_vec, [LVLH[2], 480_000, LVLH[3], 0], 500)
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
 
-plt.plot(x_cmd, -z_cmd)
-plt.xlabel("x (m)")
-plt.ylabel("altitude (m)")
-plt.title("Guidance Trajectory")
-plt.grid(True)
-plt.show()
+
+Dog1 = Dog("Mooshu", 5)
+Dog1.bark()
+print(Dog1)
+
+
+@dataclass
+class VehicleInfo:
+    state: np.ndarray
+
+    @property
+    def x(self):
+        return self.state[2]
+
+    @property
+    def inverse_x(self):
+        return -self.x
+
+
+LM = VehicleInfo(
+    np.array(
+        [
+            10,
+            20,
+            30,
+            -10,
+            500,
+        ]
+    )
+)
+
+print(LM.inverse_x)
